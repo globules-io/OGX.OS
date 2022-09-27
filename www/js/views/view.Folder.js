b1e59d8efc8a68ebe8c07dcfabb958d9070f50d3
Eric <info@ericparlier.com>
2022-09-27 19:45:24 -0400

    Folder OML

---
+++ www/js/views/view.Folder.js
@@ -0,0 +1,27 @@
+require('Views.Folder', 'View');
+OGX.Views.Folder = function(__config){
+    construct(this, 'Views.Folder');
+	'use strict'; 
+
+    //@Override
+    this.construct = function(__data, __route_data){};
+    
+    //@Override
+	this.enable = function(){};
+	
+    //@Override
+	this.disable = function(){};    
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+          
+        }else{
+            
+        }
+    }; 
+    
+    //@Override
+    this.destroy = function(){};
+
+};
