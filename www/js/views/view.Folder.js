5035bfa519f61097cebb37f94f7ef5c7289ff3a1
Eric <info@ericparlier.com>
2022-09-27 19:57:40 -0400

    Folder template

--- www/js/views/view.Folder.js
+++ www/js/views/view.Folder.js
@@ -2,9 +2,12 @@ require('Views.Folder', 'View');
 OGX.Views.Folder = function(__config){
     construct(this, 'Views.Folder');
 	'use strict'; 
+    let list;
 
     //@Override
-    this.construct = function(__data, __route_data){};
+    this.construct = function(__data, __route_data){
+        list = this.chidlren('DynamicList')[0];
+    };
     
     //@Override
 	this.enable = function(){};
@@ -15,13 +18,12 @@ OGX.Views.Folder = function(__config){
     //@Override
 	this.ux = function(__bool){
         if(__bool){
-          
+
         }else{
-            
+
         }
     }; 
     
     //@Override
     this.destroy = function(){};
-
 };
