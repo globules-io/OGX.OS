ee23532eb317c0187fb15eb8c470e49d4a3fe9e8
Eric <info@ericparlier.com>
2022-09-27 20:21:37 -0400

    FileExplorer

--- www/js/views/view.FileExplorer.js
+++ www/js/views/view.FileExplorer.js
@@ -2,11 +2,12 @@ require('Views.FileExplorer', 'View');
 OGX.Views.FileExplorer = function(__config){
     construct(this, 'Views.FileExplorer');
 	'use strict'; 
-    let tree;
+    let tree, list;
 
     //@Override
     this.construct = function(__data, __route_data){
         tree = this.children('Tree')[0];
+        list = this.children('DynamicList')[0];
     };
     
     //@Override
