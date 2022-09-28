667c642218633c1bc01d9a9edac2ed7b5159a263
Eric <info@ericparlier.com>
2022-09-27 20:05:48 -0400

    FileExplorer

---
+++ www/js/views/view.FileExplorer.js
@@ -0,0 +1,30 @@
+require('Views.FileExplorer', 'View');
+OGX.Views.FileExplorer = function(__config){
+    construct(this, 'Views.FileExplorer');
+	'use strict'; 
+    let tree;
+
+    //@Override
+    this.construct = function(__data, __route_data){
+        tree = this.children('Tree')[0];
+    };
+    
+    //@Override
+	this.enable = function(){};
+	
+    //@Override
+	this.disable = function(){};    
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+          
+        }else{
+            
+        }
+    }; 
+    
+    //@Override
+    this.destroy = function(){};
+
+};
