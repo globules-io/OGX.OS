8a552257a80ee543fe2f5d48abaf24ce28493e17
Eric <info@ericparlier.com>
2023-10-24 20:33:42 +0200

    Desktop

---
+++ www/js/views/view.Desktop.js
@@ -0,0 +1,26 @@
+require('Views.Desktop', 'View');
+OGX.Views.Desktop = function(__config){
+    construct(this, 'Views.Desktop');
+	'use strict';     
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
