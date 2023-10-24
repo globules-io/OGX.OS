ed1733732d130ddf6fbaba32a2522397ae040a90
Eric <info@ericparlier.com>
2023-10-24 22:34:02 +0200

    Settings

--- www/js/views/view.MainMenu.js
+++ www/js/views/view.MainMenu.js
@@ -15,9 +15,27 @@ OGX.Views.MainMenu = function(__config){
     //@Override
 	this.ux = function(__bool){
         if(__bool){
-            
+            this.on(this.touch.down, '.icon_settings', (__e, __item) => {
+                __e.stopImmediatePropagation();
+                this.parent.hide(true);
+                const popup = app.addPopup({
+                    id: 'settings',
+                    title: 'Settings',
+                    width: '60%',
+                    height: '60%',
+                    anim: 'scale',
+                    drag: true,
+                    resize: true,
+                    icons: [
+                        {icon:'/img/close.svg', callback:() => {
+                            app.removePopup(popup.id, false);
+                        }}
+                    ]
+                }, app.getStage().children('View')[0]);
+                popup.show(true);
+            });
         }else{
-           
+            this.off(this.touch.down, '.icon_settings');
         }
     };
 
---
+++ www/js/views/view.Settings.js
@@ -0,0 +1,26 @@
+require('Views.Settings', 'View');
+OGX.Views.Settings = function(__config){
+    construct(this, 'Views.Settings');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
