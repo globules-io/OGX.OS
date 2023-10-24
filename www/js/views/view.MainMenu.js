85e1cc9ba2e820b8e33ebc7cd3da7ebd06053413
Eric <info@ericparlier.com>
2023-10-24 20:34:07 +0200

    Main menu

---
+++ www/js/views/view.MainMenu.js
@@ -0,0 +1,26 @@
+require('Views.MainMenu', 'View');
+OGX.Views.MainMenu = function(__config){
+    construct(this, 'Views.MainMenu');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
