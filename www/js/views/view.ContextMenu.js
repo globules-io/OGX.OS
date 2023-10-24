1868c58f72fc6800db72863fd59fc4ec6d997b27
Eric <info@ericparlier.com>
2023-10-24 20:33:28 +0200

    Context menu

---
+++ www/js/views/view.ContextMenu.js
@@ -0,0 +1,26 @@
+require('Views.ContextMenu', 'View');
+OGX.Views.ContextMenu = function(__config){
+    construct(this, 'Views.ContextMenu');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
