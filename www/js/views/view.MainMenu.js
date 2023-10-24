8e85485c51b3eb1dfce4e15a47f2627a696f0031
Eric <info@ericparlier.com>
2023-10-24 23:15:37 +0200

    Programs

--- www/js/views/view.MainMenu.js
+++ www/js/views/view.MainMenu.js
@@ -2,9 +2,12 @@ require('Views.MainMenu', 'View');
 OGX.Views.MainMenu = function(__config){
     construct(this, 'Views.MainMenu');
 	'use strict'; 
+    let program_list;
 
     //@Override
-	this.construct = function(){};
+	this.construct = function(){
+        program_list = app.cfind('DynamicList', 'menu_programs');
+    };
 	
     //@Override
 	this.onFocus = function(){};
@@ -34,11 +37,44 @@ OGX.Views.MainMenu = function(__config){
                 }, app.getStage().children('View')[0]);
                 popup.show(true);
             });
+            program_list.on(OGX.DynamicList.SELECT, (__e, __item) => {
+                if(__item.type === 'MenuLetter'){
+                    return;
+                }
+                this.parent.hide(true);
+                const node = makeProgramNode(__item);
+                const popup = app.addPopup({
+                    title: __item.label,
+                    width: '60%',
+                    height: '60%',
+                    anim: 'scale',
+                    drag: true,
+                    resize: true,
+                    icons: [
+                        {icon:'/img/close.svg', callback:() => {
+                            app.removePopup(popup.id, false);
+                        }}
+                    ],
+                    'node:OML':[node]
+                }, app.getStage().children('View')[0]);
+                popup.show(true);
+            });
         }else{
             this.off(this.touch.down, '.icon_settings');
+            program_list.off(OGX.DynamicList.SELECT);
         }
     };
 
     //@Override
     this.destroy = function(){};
+
+    function makeProgramNode(__item){
+        let node = {};
+        node['default:Views.'+__item.view] = {
+            template: __item.view,
+            css: __item.view
+        };
+        console.log(node);
+        return node;
+    }
 };
