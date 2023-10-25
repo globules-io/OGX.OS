fa92677381a98d60d6889c9690315cd22e8a981e
Eric <info@ericparlier.com>
2023-10-25 11:50:14 +0200

    Refactor

---
+++ www/js/controllers/controller.ProgramManager.js
@@ -0,0 +1,85 @@
+require('Controllers.ProgramManager', 'Controller');
+OGX.Controllers.ProgramManager = function(){
+    construct(this, 'Controllers.ProgramManager');
+    'use strict';
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+
+    this.genPopup = function(__item){
+        const node = makeProgramNode(__item);
+        !__item.hasOwnProperty('group') ? __item.group = false : null;        
+        const popup = app.addPopup({
+            title: __item.label,
+            width: '60%',
+            height: '60%',
+            anim: 'scale',
+            drag: true,
+            resize: true,
+            group: __item.group,
+            maximize: true,
+            maximize_dbc: (__max) => {
+                if(__max){
+                    maximizePopup(popup);
+                }else{
+                    normalizePopup(popup);
+                }
+            },
+            icon: '/img/'+__item.icon+'.svg',
+            icons: [
+                {icon:'/img/maximize.svg', callback:maximizePopup},
+                {icon:'/img/close.svg', callback:(__popup) => {
+                    app.removePopup(__popup.id, false);
+                }}
+            ],
+            'node:OML':[node]
+        }, app.getStage().gather('Views.Desktop')[0]);
+        popup.show(true);
+    }    
+
+    function maximizePopup(__popup){    
+        __popup.maximize();        
+        let icons = __popup.icons();
+        icons[0].icon = '/img/normalize.svg';
+        icons[0].callback = normalizePopup;
+        __popup.icons(icons);
+    };    
+
+    function normalizePopup(__popup){       
+        __popup.normalize();        
+        let icons = __popup.icons();
+        icons[0].icon = '/img/maximize.svg';
+        icons[0].callback = maximizePopup;
+        __popup.icons(icons);
+    };
+
+    function makeProgramNode(__item){
+        if(__item.hasOwnProperty('oml')){
+            return app.getOML(__item.oml);
+        }
+        let node = {};
+        node['default:Views.'+__item.view] = {
+            template: __item.view,
+            css: __item.view
+        };
+        return node;
+    }    
+};
--- www/js/views/view.MainMenu.js
+++ www/js/views/view.MainMenu.js
@@ -2,12 +2,13 @@ require('Views.MainMenu', 'View');
 OGX.Views.MainMenu = function(__config){
     construct(this, 'Views.MainMenu');
 	'use strict'; 
-    let icon_list, program_list;
+    let icon_list, program_list, program_manager;
 
     //@Override
 	this.construct = function(){
         icon_list = app.cfind('DynamicList', 'menu_icons');
         program_list = app.cfind('DynamicList', 'menu_programs');
+        program_manager = app.cfind('Controller', 'program_manager');
     };
 	
     //@Override
@@ -22,14 +23,15 @@ OGX.Views.MainMenu = function(__config){
             icon_list.on(OGX.DynamicList.SELECT, (__e, __item) => {
                 __e.stopImmediatePropagation();
                 this.parent.hide(true);
-                genPopup(__item);
+                program_manager.genPopup(__item);
             });
             program_list.on(OGX.DynamicList.SELECT, (__e, __item) => {
+                __e.stopImmediatePropagation();
                 if(__item.type === 'MenuLetter'){
                     return;
                 }
                 this.parent.hide(true);                
-                genPopup(__item);
+                program_manager.genPopup(__item);
             });
         }else{
             icon_list.off(OGX.DynamicList.SELECT);
@@ -38,61 +40,5 @@ OGX.Views.MainMenu = function(__config){
     };
 
     //@Override
-    this.destroy = function(){};
-
-    function maximizePopup(__popup){    
-        __popup.maximize();        
-        let icons = __popup.icons();
-        icons[0].icon = '/img/normalize.svg';
-        icons[0].callback = normalizePopup;
-        __popup.icons(icons);
-    };    
-
-    function normalizePopup(__popup){       
-        __popup.normalize();        
-        let icons = __popup.icons();
-        icons[0].icon = '/img/maximize.svg';
-        icons[0].callback = maximizePopup;
-        __popup.icons(icons);
-    };
-
-    function makeProgramNode(__item){
-        let node = {};
-        node['default:Views.'+__item.view] = {
-            template: __item.view,
-            css: __item.view
-        };
-        return node;
-    }
-
-    function genPopup(__item){
-        const node = makeProgramNode(__item);
-        !__item.hasOwnProperty('group') ? __item.group = false : null;
-        const popup = app.addPopup({
-            title: __item.label,
-            width: '60%',
-            height: '60%',
-            anim: 'scale',
-            drag: true,
-            resize: true,
-            group: __item.group,
-            maximize: true,
-            maximize_dbc: (__max) => {
-                if(__max){
-                    maximizePopup(popup);
-                }else{
-                    normalizePopup(popup);
-                }
-            },
-            icon: '/img/'+__item.icon+'.svg',
-            icons: [
-                {icon:'/img/maximize.svg', callback:maximizePopup},
-                {icon:'/img/close.svg', callback:(__popup) => {
-                    app.removePopup(__popup.id, false);
-                }}
-            ],
-            'node:OML':[node]
-        }, app.getStage().gather('Views.Desktop')[0]);
-        popup.show(true);
-    }    
+    this.destroy = function(){};   
 };
