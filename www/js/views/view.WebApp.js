f35bd97af0c01f9a29b293678fd8a03605a11d27
Eric <info@ericparlier.com>
2023-10-25 15:04:53 +0200

    Main menu pins

---
+++ www/js/views/view.WebApp.js
@@ -0,0 +1,26 @@
+require('Views.WebApp', 'View');
+OGX.Views.WebApp = function(__config){
+    construct(this, 'Views.WebApp');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
