38554066d6355148b567200f70fb599ba3d37fa0
Eric <info@ericparlier.com>
2023-10-25 09:49:42 +0200

    Refactor popup from menu

--- www/js/views/view.MainMenu.js
+++ www/js/views/view.MainMenu.js
@@ -2,10 +2,11 @@ require('Views.MainMenu', 'View');
 OGX.Views.MainMenu = function(__config){
     construct(this, 'Views.MainMenu');
 	'use strict'; 
-    let program_list;
+    let icon_list, program_list;
 
     //@Override
 	this.construct = function(){
+        icon_list = app.cfind('DynamicList', 'menu_icons');
         program_list = app.cfind('DynamicList', 'menu_programs');
     };
 	
@@ -18,56 +19,42 @@ OGX.Views.MainMenu = function(__config){
     //@Override
 	this.ux = function(__bool){
         if(__bool){
-            this.on(this.touch.down, '.icon_settings', (__e, __item) => {
+            icon_list.on(OGX.DynamicList.SELECT, (__e, __item) => {
                 __e.stopImmediatePropagation();
                 this.parent.hide(true);
-                const popup = app.addPopup({
-                    id: 'settings',
-                    title: 'Settings',
-                    width: '60%',
-                    height: '60%',
-                    anim: 'scale',
-                    drag: true,
-                    resize: true,
-                    icons: [
-                        {icon:'/img/close.svg', callback:() => {
-                            app.removePopup(popup.id, false);
-                        }}
-                    ]
-                }, app.getStage().children('View')[0]);
-                popup.show(true);
+                genPopup(__item);
             });
             program_list.on(OGX.DynamicList.SELECT, (__e, __item) => {
                 if(__item.type === 'MenuLetter'){
                     return;
                 }
-                this.parent.hide(true);
-                const node = makeProgramNode(__item);
-                const popup = app.addPopup({
-                    title: __item.label,
-                    width: '60%',
-                    height: '60%',
-                    anim: 'scale',
-                    drag: true,
-                    resize: true,
-                    icon: '/img/'+__item.icon+'.svg',
-                    icons: [
-                        {icon:'/img/close.svg', callback:() => {
-                            app.removePopup(popup.id, false);
-                        }}
-                    ],
-                    'node:OML':[node]
-                }, app.getStage().children('View')[0]);
-                popup.show(true);
+                this.parent.hide(true);                
+                genPopup(__item);
             });
         }else{
-            this.off(this.touch.down, '.icon_settings');
+            icon_list.off(OGX.DynamicList.SELECT);
             program_list.off(OGX.DynamicList.SELECT);
         }
     };
 
     //@Override
     this.destroy = function(){};
+
+    function maximizePopup(__popup){    
+        __popup.maximize();        
+        let icons = __popup.icons();
+        icons[0].icon = '/img/normalize.svg';
+        icons[0].callback = normalizePopup;
+        __popup.icons(icons);
+    };    
+
+    function normalizePopup(__popup){       
+        __popup.normalize();        
+        let icons = __popup.icons();
+        icons[0].icon = '/img/maximize.svg';
+        icons[0].callback = maximizePopup;
+        __popup.icons(icons);
+    };
 
     function makeProgramNode(__item){
         let node = {};
@@ -77,4 +64,34 @@ OGX.Views.MainMenu = function(__config){
         };
         return node;
     }
+
+    function genPopup(__item){
+        const node = makeProgramNode(__item);
+        console.log(node);
+        const popup = app.addPopup({
+            title: __item.label,
+            width: '60%',
+            height: '60%',
+            anim: 'scale',
+            drag: true,
+            resize: true,
+            maximize: true,
+            maximize_dbc: (__max) => {
+                if(__max){
+                    maximizePopup(popup);
+                }else{
+                    normalizePopup(popup);
+                }
+            },
+            icon: '/img/'+__item.icon+'.svg',
+            icons: [
+                {icon:'/img/maximize.svg', callback:maximizePopup},
+                {icon:'/img/close.svg', callback:() => {
+                    app.removePopup(popup.id, false);
+                }}
+            ],
+            'node:OML':[node]
+        }, app.getStage().children('View')[0]);
+        popup.show(true);
+    }    
 };
