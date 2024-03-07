c1661c5660dfdaa051d71cd9a62c5c099ee09751
Eric <info@ericparlier.com>
2024-03-07 16:50:41 +0100

    DataManager

--- www/js/controllers/controller.DataManager.js
+++ www/js/controllers/controller.DataManager.js
@@ -9,28 +9,18 @@ OGX.Controllers.DataManager = function(){
         let json;
         const drives = this.getDrives();
         if(!drives.length){
-            json = app.getJSON('drives');
-            mongogx.setCollection('drives');
-            json.forEach(__drive => {
-                mongogx.insert(__drive);
-            });
-        }   
-        const programs = this.getPrograms();
-        if(!programs.length){
-            json = app.getJSON('programs');
-            mongogx.setCollection('menu_programs');
-            json.forEach(__program => {
-                mongogx.insert(__program);
-            });
-        }
+            json = {label : 'System', letter : 'C', size : getStorageSize(), used: 0};
+            mongogx.setCollection('drives');    
+            mongogx.insert(json);            
+        }  
         const pins = this.getPins();
         if(!pins.length){
-            json = app.getJSON('pins');
+            json = app.getJSON('menu_pins');
             mongogx.setCollection('menu_pins');
             json.forEach(__pin => {
                 mongogx.insert(__pin);
             });
-        }
+        }        
     };
 	
     //@Override
@@ -51,15 +41,11 @@ OGX.Controllers.DataManager = function(){
     //@Override
     this.destroy = function(){};
 
+    /* DRIVES */
     this.getDrives = function(){
         mongogx.setCollection('drives');
         return mongogx.find({});
-    };
-
-    this.getPrograms = function(){
-        mongogx.setCollection('menu_programs');
-        return mongogx.find({});
-    };
+    };   
 
     this.getPins = function(){
         mongogx.setCollection('menu_pins');
@@ -70,13 +56,51 @@ OGX.Controllers.DataManager = function(){
 
     };
 
+    this.getFiles = function(__drive, __path){
+
+    };
+
+    //fetch everything from localStorage that is not mongoogx
+    //substract to total
+    function getDriveUse(__letter){        
+        let total = getStorageUse('mongogx');
+        let size = 0;
+    }
+
+    //total storage used
+    function getStorageUse(__skip){
+        let size = 0;
+        let a;
+        for(a in localStorage){
+            if(a !== __skip){
+                size += localStorage[a].length;
+            }
+        }
+        return size;
+    }
+
+    function getStorageSize(){       
+        let i;
+        let size = 0;
+        try {
+            for (i = 250; i <= 10000; i += 250) {
+                localStorage.setItem('o', new Array((i * 1024) + 1).join('o'));
+            }
+        } catch (e) {
+            localStorage.removeItem('o');     
+            size = i - 250;     
+        }   
+        return size;     
+    }
+
     function initMongo(){
 		let options = {encryption:{scheme:OGX.Mongogx.ENCRYPTION_AES, key:'ogx'}};		
-        mongogx = new OGX.Mongogx(null, null, options);
-        mongogx.setDatabase('system');	        
+        mongogx = new OGX.Mongogx(null, null, options);     
         mongogx.createDatabase('system');
         mongogx.setDatabase('system');	
+        //refactor
         mongogx.createCollection('drives');
+        mongogx.createCollection('files');
         mongogx.createCollection('menu_programs');
         mongogx.createCollection('menu_pins');        
 	} 	
