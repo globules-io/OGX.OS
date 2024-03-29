07669854a9fcba63c3515513ba65e426a312cf6d
Eric <info@ericparlier.com>
2024-03-29 15:32:53 +0100

    Display Preferences

---
+++ www/js/views/view.DisplayPreferences.js
@@ -0,0 +1,40 @@
+require('Views.DisplayPreferences', 'View');
+OGX.Views.DisplayPreferences = function(__config){
+    construct(this, 'Views.DisplayPreferences');
+	'use strict'; 
+    let list, roulette;
+
+    //@Override
+	this.construct = function(){
+        list = app.cfind('DynamicList', 'swatch_list');
+        roulette = app.cfind('Roulette', 'background_mode');
+    };
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+
+    //@Override
+	this.onResize = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            roulette.on(OGX.Roulette.CHANGE, (__e, __val) => {
+                this.el.find('.box').addClass('hidden');
+                this.el.find('.'+__val).removeClass('hidden');
+            });
+            list.on(OGX.DynamicList.SELECT, (__e, __swatch) => {
+                app.getStage().gather('Views.Desktop')[0].color(__swatch.color);
+            });
+        }else{
+            roulette.off(OGX.Roulette.CHANGE);
+            list.off(OGX.DynamicList.SELECT);
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
