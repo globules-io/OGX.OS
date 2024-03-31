8673f302a05a220174e05840fa852c4b820e53fa
Eric <info@ericparlier.com>
2024-03-31 15:01:09 +0200

    TextEditor

--- www/js/views/view.TextEditor.js
+++ www/js/views/view.TextEditor.js
@@ -2,9 +2,45 @@ require('Views.TextEditor', 'Program', 'View');
 OGX.Views.TextEditor = function(__config){
     construct(this, 'Views.TextEditor');
 	'use strict'; 
-
+    let popup;
+   
     //@Override
-	this.construct = function(){};
+	this.construct = function(){
+        popup = app.findPopup(this);
+        tinymce.init({
+            selector:'textarea.editor',
+            plugins: 'image lists',
+            menubar:'',
+            toolbar:'undo redo | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | bullist | customimage customvideo customlink customtag',
+            resize:false,
+            skin:'ogx',
+            content_css:'dark',            
+            setup:function(ed){
+                ed.on('keyup', onBodyChange);                             
+            },     
+            file_picker_callback: function (cb, value, meta) {
+                image = false;
+                const input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');                
+                input.onchange = function(){                   
+                    let file = this.files[0];
+                    const reader = new FileReader();
+                    reader.onload = function(){                       
+                        let id = 'blobid' + (new Date()).getTime();
+                        let blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                        const base64 = image = reader.result.split(',')[1];   
+                        const blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);                       
+                        cb(blobInfo.blobUri(), { title: file.name });
+                    };
+                    reader.readAsDataURL(file);
+                };
+                input.click();
+            }
+        });   
+        
+    };
 	
     //@Override
 	this.onFocus = function(){};
@@ -22,5 +58,16 @@ OGX.Views.TextEditor = function(__config){
     };
 
     //@Override
-    this.destroy = function(){};
+    this.destroy = function(){
+        tinymce.remove('textarea.editor');
+    };
+
+    this.val = function(__string){
+       
+    };
+
+    function onBodyChange(){
+
+    }
 };
+    
