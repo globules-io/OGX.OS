9c82c929ca18ad49faa1f9ae6307eb730daf642d
Eric <info@ericparlier.com>
2024-03-31 16:00:20 +0200

    Missing Camera app

---
+++ www/js/views/view.Camera.js
@@ -0,0 +1,29 @@
+require('Views.Camera', 'View');
+OGX.Views.Camera = function(__config){
+    construct(this, 'Views.Camera');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+
+    //@Override
+	this.onResize = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
