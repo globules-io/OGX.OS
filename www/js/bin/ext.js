e87043c08dac944cb0419501888edcb8bbb8af24
Eric <info@ericparlier.com>
2024-03-31 12:42:50 +0200

    Extensions

--- www/js/bin/ext.js
+++ www/js/bin/ext.js
@@ -1,4 +1,30 @@
 /* EXTENDING OGX CLASSES */
+/* OS */
+require('OS', 'Core');
+OGX.OS = function(__config){
+    construct(this, 'OS');
+    'use strict'; 
+
+    /* NAME SPACE */
+    this.SYSTEM = {};
+
+    /* FILE MANAGER */
+    this.SYSTEM.FILE = {};
+    this.SYSTEM.FILE.create = function(__path, __name){};
+    this.SYSTEM.FILE.delete = function(__path){};
+    this.SYSTEM.FOLDER = {};
+    this.SYSTEM.FOLDER.create = function(__path, __name){};
+    this.SYSTEM.FOLDER.delete = function(__path){};
+
+    /* PROGRAM MANAGER */
+    this.SYSTEM.PROCESS = {};
+    this.SYSTEM.PROCESS.start = function(__name, __desktop){};
+    this.SYSTEM.PROCESS.stop = function(__process_id){};
+
+};
+
+/* PROGRAMS */
+require('Program');
 OGX.Program = function(__config){ 
     construct(this, 'Program');
     'use strict'; 
