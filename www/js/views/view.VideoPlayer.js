9aff36d9c941ab012653178b6cca32faa16d74e2
Eric <info@ericparlier.com>
2024-03-31 16:24:52 +0200

    VideoPlayer

---
+++ www/js/views/view.VideoPlayer.js
@@ -0,0 +1,29 @@
+require('Views.VideoPlayer', 'View');
+OGX.Views.VideoPlayer = function(__config){
+    construct(this, 'Views.VideoPlayer');
+	'use strict'; 
+
+    //@Override
+	this.construct = function(){};
+	
+    //@Override
+	this.onFocus = function(){};
+	
+    //@Override
+	this.onBlur = function(){};
+
+    //@Override
+	this.onResize = function(){};
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+            
+        }else{
+           
+        }
+    };
+
+    //@Override
+    this.destroy = function(){};
+};
