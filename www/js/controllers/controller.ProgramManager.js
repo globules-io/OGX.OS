07dc60f19f80047ff38fca192dbd6bac40a125f9
Eric <info@ericparlier.com>
2024-04-02 18:22:09 +0200

    https://github.com/globules-io/OGX.OS/issues/11

--- www/js/controllers/controller.ProgramManager.js
+++ www/js/controllers/controller.ProgramManager.js
@@ -2,35 +2,38 @@ require('Controllers.ProgramManager', 'Controller');
 OGX.Controllers.ProgramManager = function(){
     construct(this, 'Controllers.ProgramManager');
     'use strict';
+    let registered_programs;
 
     //@Override
-	this.construct = function(){};
-	
-    //@Override
-	this.onFocus = function(){};
-	
-    //@Override
-	this.onBlur = function(){};
-	
-    //@Override
-	this.ux = function(__bool){
-        if(__bool){
-            
-        }else{
-           
-        }
+	this.construct = function(){
+        registered_programs = app.getJSON('programs');
+
+        app.on(OGX.Popup.GROUP, (__e, __o) => {
+            let icons = __o.popup.icons();
+            icons[icons.length-1].callback = app.SYSTEM.PROCESS.stop;
+            icons[icons.length-1].params = __o.popup;
+            __o.popup.icons(icons);
+        });
+        app.on(OGX.Popup.UNGROUP, (__e, __o) => {
+            const new_popup = app.cfind('Popup', __o.to_popup);
+            let icons = new_popup.icons();
+            icons[icons.length-1].callback = app.SYSTEM.PROCESS.stop;
+            icons[icons.length-1].params = new_popup.children()[0].id;
+            new_popup.icons(icons);
+        });
     };
-
-    //@Override
-    this.destroy = function(){};
 
     this.genPopup = function(__desktop, __item, __data){
         typeof __data === 'undefined' ? __data = {} : null;
-        const node = makeProgramNode(__item, __data);        
+        const node = makeProgramNode(__item, __data);  
+        if(!node){
+            return;
+        }      
         
         //test uniqueness       
         if(__item.hasOwnProperty('config') && __item.config.hasOwnProperty('unique') && __item.config.unique){
             const cls = OGX.OML.getNodeClass(node);
+            //process
             const instance = app.gather(cls);
             if(instance.length){
                 instance[0].reveal();
@@ -51,22 +54,26 @@ OGX.Controllers.ProgramManager = function(){
             icons: [
                 {icon:'/img/minimize.svg', callback:minimizePopup},
                 {icon:'/img/maximize.svg', callback:maximizePopup},
-                {icon:'/img/close.svg', callback:(__popup) => {
-                    app.SYSTEM.PROCESS.stop(__popup.id);
-                }}
+                //would be better to set process id from App and not popup because grouping
+                //I don't know yet the id of the process cause it's in popup after creating
+                {icon:'/img/close.svg', callback: app.SYSTEM.PROCESS.stop}
             ],
             'node:OML':[node]
         };
         
-        if(__item.hasOwnProperty('config')){            
-            OGX.Data.merge(options, __item.config, true);
+        if(registered_programs[__item.app].hasOwnProperty('config')){            
+            OGX.Data.merge(options, registered_programs[__item.app].config, true);
         }
         options.icon = '/img/'+__item.icon+'.svg';
         options.title = __item.label;
         const popup = app.addPopup(options, __desktop);
+        const process = popup.children('View')[0];
+        let icons = popup.icons();
+        icons[icons.length-1].params = process.id;
+        popup.icons(icons)
         app.cfind('Docker', 'docker').addItem(popup, {icon: popup.icon()});
         popup.show(true);
-        return popup;
+        return process;
     }    
 
     function minimizePopup(__popup){
@@ -90,8 +97,12 @@ OGX.Controllers.ProgramManager = function(){
     };
 
     function makeProgramNode(__item, __data){
-        if(__item.hasOwnProperty('oml')){
-            let oml = app.getOML(__item.oml);           
+        if(!registered_programs.hasOwnProperty(__item.app)){
+            return;
+        }
+        let conf = registered_programs[__item.app];             
+        if(conf.hasOwnProperty('oml')){
+            let oml = app.getOML(conf.oml);           
             if(__data){
                 for(let n in oml){
                     n.data = __data;
@@ -101,8 +112,8 @@ OGX.Controllers.ProgramManager = function(){
             return oml;           
         }
         let node = {};
-        node['default:Views.'+__item.view] = {
-            template: __item.view,
+        node['default:Views.'+__item.app] = {
+            template: __item.app,
             data: __data
         };
         return node;
